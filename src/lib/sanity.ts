f5cbdc6f2eac533d14e63a58fb8f0918716b07c3
Michele Portas <mportas@lebensmittel-sonderposten.de>
2026-07-24 14:06:44 +0200

    Fix dataset Sanity

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -2,7 +2,7 @@ import { createClient } from "@sanity/client";
 
 export const sanity = createClient({
   projectId: "2lm7s5yj",
-  dataset: "production",
+  dataset: "berg-buben-sanity",
   apiVersion: "2024-10-01",
   useCdn: true,
 });
